8ec0ed2cbecd81d00201f52b4761e53d5970a142
Hendrik Kurniawan <hendrik.kurniawan@rtl.nl>
2015-12-29 17:09:16 +0100

    adds eslinter plugin

--- components/HelloMessage.js
+++ components/HelloMessage.js
@@ -2,18 +2,18 @@ import React from 'react'
 import { Link } from 'react-router'
 
 export default class HelloMessag extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello world</h1>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About (lazy loaded)</Link></li>
-        </ul>
-        {this.props.children}
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <h1>Hello world</h1>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About (lazy loaded)</Link></li>
+                </ul>
+                {this.props.children}
+            </div>
+        )
+    }
 }
 
 
--- server/app.js
+++ server/app.js
@@ -1,3 +1,5 @@
+'use strict;'
+
 import path from 'path'
 import fs from 'fs'
 import _ from 'lodash'
@@ -8,36 +10,44 @@ import { renderToString } from 'react-dom/server'
 import { match, RoutingContext } from 'react-router'
 import routes from './../route'
 
-var proxy = httpProxy.createProxyServer();
-var isProduction = process.env.NODE_ENV === 'production';
-var port = isProduction ? process.env.PORT : 3000;
-var publicPath = path.resolve(__dirname, './../www');
-var template = fs.readFileSync(publicPath + '/index.html', 'utf8');
-var server = http.createServer((req, res) => {
+let proxy = httpProxy.createProxyServer(),
+    isProduction = process.env.NODE_ENV === 'production',
+    port = isProduction ? process.env.PORT : 3000,
+    publicPath = path.resolve(__dirname, './../www'),
+    template = fs.readFileSync(publicPath + '/index.html', 'utf8')
+
+const server = http.createServer((req, res) => {
     match({ routes, location: req.url }, (error, redirectLocation, renderProps) => {
-    
+
         if (error) {
             console.log('terrible' + error);
             //writeError('ERROR!', res)
+        
         } else if (redirectLocation) {
             console.log('redirect');
             //redirect(redirectLocation, res)
+
         } else if (renderProps) {
-            res.setHeader('Content-Type', 'text/html');
-            var data = {};
+            let data = {},
+                html = _.template(template);
+            res.setHeader('Content-Type', 'text/html'); 
             data.body = renderToString(<RoutingContext {...renderProps} />);
-            var html = _.template(template);
-            res.end(html(data));  
+            res.end(html(data));
+            
         } else {
             console.log('not found, though not entirely');
-            res.end();
+            
+            // this feels as a hacky way to distinct dev from prod server-logic
+            if(isProduction) {
+                res.end();
+            }
             //  writeNotFound(res)
         }
     })
 })
 
-if(!isProduction){
-    var bundle = require('./../webpack/hot-server');
+if(!isProduction) {
+    let bundle = require('./../webpack/hot-server');
     bundle.default();
     server.on('request', function(req, res){
         if(req.url === '/bundle.js'){
