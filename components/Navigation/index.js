6db629fbb91b3b3b7d69e355cd5272998221f3c6
Hendrik Kurniawan <hendrik.kurniawan@rtl.nl>
2016-01-06 23:50:47 +0100

    fleshing out the demo

--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -9,6 +9,9 @@ class Navigation extends React.Component {
     render() {
         return (
             <div>
+                <header>
+                    <h2>React SSR demo</h2>
+                </header>
                 <ul className='navbar'>
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/bye">Bye message</Link></li>
